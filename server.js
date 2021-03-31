b6c3fd1e7a412bfd21e127c640d1cc819511c7ec
Jeremiaszmacura <jeremiaszmacura99@gmail.com>
2021-03-31 13:32:34 +0200

    connected to database mongoDB

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const mongoose = require('mongoose');
 const app = express(); // create app
 
 
+// connect to mongoDB
+const dbURL = 'mongodb+srv://jeremiasz:jeremiasz1234@discipuluscluster.dvdwf.mongodb.net/Discipulus?retryWrites=true&w=majority';
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000)) // dopiero po połączenu z baza danych zaczynamy nasłuchiwanie
+    .catch((err) => console.log(err));
+
+
 app.engine('hbs', expressHandlebars({
     defaultLayout: 'main',
     extname: 'hbs'
